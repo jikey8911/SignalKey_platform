bc98a8b88621be0ce0af0405414cf3629cda3b50
juank8911 <ingjuanguzmane@gmail.com>
2026-01-29 18:30:46 -0500

    tr

--- web/client/src/lib/trpc.ts
+++ web/client/src/lib/trpc.ts
@@ -1,4 +1,13 @@
 import { createTRPCReact } from "@trpc/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import type { AppRouter } from "../../../server/routers";
 
 export const trpc = createTRPCReact<AppRouter>();
+
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+});
